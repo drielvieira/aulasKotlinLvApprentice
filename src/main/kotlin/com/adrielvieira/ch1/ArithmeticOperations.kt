b5ac736d95d14d929c6372c64a3ae06d1537949a
adriel vieira <adriel532vieira@gmail.com>
2021-12-14 09:27:58 -0300

    adici mais arqi...

--- src/main/kotlin/com/adrielvieira/ch1/ArithmeticOperations.kt
+++ src/main/kotlin/com/adrielvieira/ch1/ArithmeticOperations.kt
@@ -12,4 +12,17 @@ fun main() {
 
     println( 28 % 10)  // resto
     println(28.0 % 10.0)
+
+    println("%.0f" .format(28.0 % 10.0))
+
+    /*
+        //Shift left: shl
+        //Shift right: shr
+    * são funções infixas que você coloca entre os
+    * operadores para que a chamada da função pareça uma
+    * operação.
+    * */
+
+    println(1 shl 3) // facilita a multiplicação ou divisão por potências de dois.
+    println(32 shr 2)
 }
---
+++ src/main/kotlin/com/adrielvieira/ch1/OrderPrecedence.kt
@@ -0,0 +1,11 @@
+package com.adrielvieira.ch1
+
+fun main() {
+    /*
+    * operações em ordem de procedencia.
+    * */
+    println((8000 / (5 * 10) - 32) shr (29 % 5))
+
+    println(350 / 5 + 2)
+    println(350 / (5 + 2))
+}
